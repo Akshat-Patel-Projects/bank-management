b97615e5ad95e42977828f0967c2caf2001e0226
AkshatPat3 <akshatpat3l@gmail.com>
2025-03-12 17:51:16 -0400

    -Initital commit: Setup server and database

---
+++ server.js
@@ -0,0 +1,33 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+// 1. Import dependencies
+import express from 'express';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+
+// 2. Load environment variables
+dotenv.config();
+
+// 3. Initialize Express app
+const app = express();
+
+// 4. Middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+// 5. Connect to MongoDB
+mongoose.connect('mongodb+srv://akshatpat3l:Jq15GAayffjxtqKx@bank.3y4aj.mongodb.net/bankDB?retryWrites=true&w=majority&appName=Bank')
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log('Error connecting to MongoDB:', err));
+
+
+
+// 6. Default route
+app.get('/', (req, res) => {
+  res.send('Bank Management API is running...');
+});
+
+// 7. Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
